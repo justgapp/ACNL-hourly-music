a9f5ae108e92d2b9acd9d9d4c5712d41f5870753
Léo M <54221236+LeoandCode@users.noreply.github.com>
2020-05-06 17:11:21 +0200

    update !

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 var slider = tns({
     container: '.my-slider',
     items:1,
-    center:true
+    gutter:2000,
+    center:true,
+    controls:false,
+    autoWidth:true,
+    nav:false,
+    arrowKeys:true,
+    autoplay:true,
+    loop:true,
+    autoplayHoverPause:true,
+    lazyload:true,
+    speed:750
   });
 
-let heure = new Date().getHours()
+const songTitle = document.querySelector('#title')
+const songHour = document.querySelector('#hour')
 const audio = document.querySelector('#audio')
 audio.volume = 0.25
-
+  
+let heure = new Date().getHours()
 let temps = ''
 
 const inputHeure = document.querySelector('#heure')
@@ -20,70 +32,217 @@ buttonHeure.addEventListener('click', ()=>{
     startSong()
 })
 
-const songTitle = document.querySelector('#title')
+const selectMeteo = document.querySelector('#meteo')
+selectMeteo.addEventListener('change', e=>{
+    temps = e.target.value
+})
+
+const buttonMeteo = document.querySelector('#confm')
+buttonMeteo.addEventListener('click', ()=>{
+    startSong()
+})
+
 
 function startSong() {
     audio.setAttribute('src',`ACNL/${pickSongName()}.mp3`)
     songTitle.innerText = pickSongName()
+    songHour.innerText = heure<=23 ? `(${heure}h)` : ''
     // audio.play()
 }
 
 
 function pickSongName() {
     if (heure === 1) {
-        if(temps === "pluvieux") {
+        if(temps === "pluie") {
             return '038 - 1 AM - Rainy'
-        } else if (temps === 'enneigé') {
+        } else if (temps === 'neige') {
             return '062 - 1 AM - Snowy'
         } else {
             return '014 - 1 AM'
         }
     } else if (heure === 2) {
-        return '015 - 2 AM'
+        if(temps === "pluie"){
+            return '039 - 2 AM - Rainy'
+        }else if(temps === 'neige'){
+            return '063 - 2 AM - Snowy'
+        }else{
+            return '015 - 2 AM'
+        } 
     } else if (heure === 3) {
-        return '016 - 3 AM'
+        if(temps === "pluie") {
+            return '040 - 3 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '064 - 3 AM - Snowy'
+        } else {
+            return '016 - 3 AM'
+        }
     } else if (heure === 4) {
-        return '017 - 4 AM'
+        if(temps === "pluie") {
+            return '041 - 4 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '065 - 4 AM - Snowy'
+        } else {
+            return '017 - 4 AM'
+        }
     } else if (heure === 5) {
-        return '018 - 5 AM'
+        if(temps === "pluie") {
+            return '042 - 5 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '066 - 5 AM - Snowy'
+        } else {
+            return '018 - 5 AM'
+        }
     } else if (heure === 6) {
-        return '019 - 6 AM'
+        if(temps === "pluie") {
+            return '043 - 6 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '067 - 6 AM - Snowy'
+        } else {
+            return '019 - 6 AM'
+        }
     } else if (heure === 7) {
-        return '020 - 7 AM'
+        if(temps === "pluie") {
+            return '044 - 7 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '068 - 7 AM - Snowy'
+        } else {
+            return '020 - 7 AM'
+        }
     } else if (heure === 8) {
-        return '021 - 8 AM'
+        if(temps === "pluie") {
+            return '045 - 8 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '069 - 8 AM - Snowy'
+        } else {
+            return '021 - 8 AM'
+        }
     } else if (heure === 9) {
-        return '022 - 9 AM'
+        if(temps === "pluie") {
+            return '046 - 9 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '070 - 9 AM - Snowy'
+        } else {
+            return '022 - 9 AM'
+        }
     } else if (heure === 10) {
-        return '023 - 10 AM'
+        if(temps === "pluie") {
+            return '047 - 10 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '071 - 10 AM - Snowy'
+        } else {
+            return '023 - 10 AM'
+        }
     } else if (heure === 11) {
-        return '024 - 11 AM'
+        if(temps === "pluie") {
+            return '048 - 11 AM - Rainy'
+        } else if (temps === 'neige') {
+            return '072 - 11 AM - Snowy'
+        } else {
+            return '024 - 11 AM'
+        }
     } else if (heure === 12) {
-        return '025 - 12 PM'
+        if(temps === "pluie") {
+            return '049 - 12 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '073 - 12 PM - Snowy'
+        } else {
+            return '025 - 12 PM'
+        }
     } else if (heure === 13) {
-        return '026 - 1 PM'
+        if(temps === "pluie") {
+            return '050 - 1 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '074 - 1 PM - Snowy'
+        } else {
+            return '026 - 1 PM'
+        }
     } else if (heure === 14) {
-        return '027 - 2 PM'
+        if(temps === "pluie") {
+            return '051 - 2 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '075 - 2 PM - Snowy'
+        } else {
+            return '027 - 2 PM'
+        }
     } else if (heure === 15) {
-        return '028 - 3 PM'
+        if(temps === "pluie") {
+            return '052 - 3 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '076 - 3 PM - Snowy'
+        } else {
+            return '028 - 3 PM'
+        }
     } else if (heure === 16) {
-        return '029 - 4 PM'
+        if(temps === "pluie") {
+            return '053 - 4 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '077 - 4 PM - Snowy'
+        } else {
+            return '029 - 4 PM'
+        }
     } else if (heure === 17) {
-        return '030 - 5 PM'
+        if(temps === "pluie") {
+            return '054 - 5 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '078 - 5 PM - Snowy'
+        } else {
+            return '030 - 5 PM'
+        }
     } else if (heure === 18) {
-        return '031 - 6 PM'
+        if(temps === "pluie") {
+            return '055 - 6 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '079 - 6 PM - Snowy'
+        } else {
+            return '031 - 6 PM'
+        }
     } else if (heure === 19) {
-        return '032 - 7 PM'
+        if(temps === "pluie") {
+            return '056 - 7 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '080 - 7 PM - Snowy'
+        } else {
+            return '032 - 7 PM'
+        }
     } else if (heure === 20) {
-        return '033 - 8 PM'
+        if(temps === "pluie") {
+            return '057 - 8 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '081 - 8 PM - Snowy'
+        } else {
+            return '033 - 8 PM'
+        }
     } else if (heure === 21) {
-        return '034 - 9 PM'
+        if(temps === "pluie") {
+            return '058 - 9 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '082 - 9 PM - Snowy'
+        } else {
+            return '034 - 9 PM'
+        }
     } else if (heure === 22) {
-        return '035 - 10 PM'
+        if(temps === "pluie") {
+            return '059 - 10 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '083 - 10 PM - Snowy'
+        } else {
+            return '035 - 10 AM'
+        }
     } else if (heure === 23) {
-        return '036 - 11 PM'
+        if(temps === "pluie") {
+            return '060 - 11 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '084 - 11 PM - Snowy'
+        } else {
+            return '036 - 11 PM'
+        }
     } else if (heure === 0) {
-        return '037 - 12 AM'
+        if(temps === "pluie") {
+            return '061 - 12 PM - Rainy'
+        } else if (temps === 'neige') {
+            return '085 - 12 PM - Snowy'
+        } else {
+            return '037 - 12 PM'}
     } else {
         return '185 - SFX - Pranked'
     }
